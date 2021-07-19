985641c76c355d84ba579f86b65916c0ac72a8a9
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-19 19:17:10 +0300

    country container layout

--- src/components/countries-container/CountriesContainer.js
+++ src/components/countries-container/CountriesContainer.js
@@ -1,6 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+import CountryCard from "./country-card/CountryCard";
 
 import "./Countries.style.scss";
 export default function CountriesContainer() {
-  return <div className="countries">Countries</div>;
+  const [countryList, setCountryList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://restcountries.eu/rest/v2/all?fields=name;capital;languages;population;region;flag"
+      )
+      .then(({ data }) => {
+        setCountryList(data);
+      });
+  }, []);
+
+  return (
+    <div className="countries">
+      <div className="countries__filter">FILTER HERE</div>
+      <div className="countries__container">
+        {countryList.map((country, index) => {
+          if (index > 230) {
+            return <CountryCard cardData={country} />;
+          }
+        })}
+      </div>
+      <div className="countries__paging">PAGES HERE</div>
+    </div>
+  );
 }
