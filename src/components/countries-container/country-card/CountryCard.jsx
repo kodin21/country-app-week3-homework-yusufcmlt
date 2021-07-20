04df403b04bf411e6b33d94caedf17750f805165
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-20 18:08:03 +0300

    animations on mount

--- src/components/countries-container/country-card/CountryCard.jsx
+++ src/components/countries-container/country-card/CountryCard.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import '../Countries.style.scss';
+import "../Countries.style.scss";
 
 export default function CountryCard({ cardData }) {
   const { name, languages, population, region, flag, capital } = cardData;
+  const [createAnimation, setAnimation] = useState("");
+
+  useEffect(() => {
+    let randomTimeout = createRandomAnimation();
+
+    return () => {
+      clearTimeout(randomTimeout);
+    };
+  }, []);
+
+  //Add creation animation with random delay
+  const createRandomAnimation = () => {
+    let randomTime = Math.round(Math.random() * 1500) + 1;
+    let creatorTimeout = setTimeout(() => {
+      setAnimation(" card--animation");
+    }, randomTime);
+
+    return creatorTimeout;
+  };
 
   return (
-    <div className="card">
+    <div className={`card${createAnimation}`}>
       <h3 className="card__heading">{name}</h3>
       <img src={flag} alt={`${name} flag`} className="card__flag" />
       <div className="card__content">
