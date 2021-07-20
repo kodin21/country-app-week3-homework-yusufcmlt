d1e53b2ec769293b973a8cdf10664bd755c60366
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-20 18:08:18 +0300

    created statistics page layout

--- src/components/statistics-container/StatisticsContainer.jsx
+++ src/components/statistics-container/StatisticsContainer.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { useFadeAnimation } from "../../utils/custom-hooks";
+
+import "./Statistics.style.scss";
 
 export default function StatisticsContainer() {
-  return <div></div>;
+  const { fadeAnimation } = useFadeAnimation(" statistics--enter-left");
+  return (
+    <div className={`statistics${fadeAnimation}`}>
+      <h2 className="statistics__heading">Statistics</h2>
+      <div className="placeholderTest">TEST</div>
+    </div>
+  );
 }
